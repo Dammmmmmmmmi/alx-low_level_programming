de6a3202442515031e8b3c81a0fabac1059f996e
Dammmmmmmmmi <damilolaanita@gmail.com>
2022-09-08 17:53:33 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - print the string in the put funcion
  *
- *  * main - print the size of various types
- *
- *   *
- *
- *    * Description: prints the size of various types on the computer it is compiled
- *
- *     * and run on
- *
- *      *
- *
- *       * Return: Always 0 (Success)
- *
- *        */
-
-
-
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
-
 {
+	char c;
+	 int i;
+	 long li;
+	 long long lli;
+	  float f;
 
-		printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-
-			printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-
-				printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-
-					printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-
-						printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-
-
-
-							return (0);
-
+	   printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	   printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	    printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	     printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	     printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	     return (0);
 }
